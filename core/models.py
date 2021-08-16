d31af3bc58aec8a3f4eb09ee5b64344ebf52e776
Marcos Beraldo Barros <marcosberaldobarros@hotmail.com>
2021-08-16 17:59:26 -0400

    Pequena mudança no models do core

--- core/models.py
+++ core/models.py
@@ -13,7 +13,7 @@ class PontoTuristico(models.Model):
     atracoes = models.ManyToManyField(Atracao)
     comentarios = models.ManyToManyField(Comentario)
     avaliacao = models.ManyToManyField(Avaliacao)
-    endereco = models.ForeignKey(Endereco, on_delete=models.CASCADE)
+    endereco = models.ForeignKey(Endereco, on_delete=models.CASCADE, null=True, blank=True)
 
 
     def __str__(self):
