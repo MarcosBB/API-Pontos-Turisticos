cdfc34a3783c73f2108c93d0cf9aa0d10cc335cc
Marcos Beraldo Barros <marcosberaldobarros@hotmail.com>
2021-08-17 15:20:34 -0400

    adicionando endpoint de atrações

---
+++ atracoes/api/serializers.py
@@ -0,0 +1,8 @@
+from atracoes.models import Atracao
+from rest_framework.serializers import ModelSerializer
+from atracoes.models import Atracao
+
+class AtracaoSerializer(ModelSerializer):
+    class Meta:
+        model = Atracao
+        fields = ('id','nome', 'descricao', 'horario_func', 'idade_minima')
--- pontos_turisticos/urls.py
+++ pontos_turisticos/urls.py
@@ -18,9 +18,11 @@ from django.urls import path
 from django.urls.conf import include
 from rest_framework import routers
 from core.api.viewsets import PontoTuristicoViewSet
+from atracoes.api.viewsets import AtracoesViewSet
 
 router = routers.DefaultRouter()
 router.register(r'pontoturistico', PontoTuristicoViewSet)
+router.register(r'atracoes', AtracoesViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
