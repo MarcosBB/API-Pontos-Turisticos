6c84c6ba96f73f8a97c4c87c7226b5cf945ac134
Marcos Beraldo Barros <marcosberaldobarros@hotmail.com>
2021-08-18 16:24:28 -0400

    Como modificar actions do rest_framework ou criar novas

--- core/api/viewsets.py
+++ core/api/viewsets.py
@@ -2,8 +2,28 @@ from rest_framework.viewsets import ModelViewSet
 from core.models import PontoTuristico
 from .serializers import PontoTuristicoSerializer
 
+
 class PontoTuristicoViewSet(ModelViewSet):
     serializer_class = PontoTuristicoSerializer
 
     def get_queryset(self):
         return PontoTuristico.objects.filter(aprovado=True)
+
+    ''' PARA SOBRESCREVER ACTIONS DO REST
+    
+    def list(self, request, *args, **kwargs):
+        return Response({'teste':'123'})
+    
+
+    PODEMOS SOBRESCREVER OS MÉTODOS list, create, destroy, retrieve, uptade, partial_update e etc
+    '''
+
+
+    ''' PARA CRIAR ACTIOS DIFERENTES
+    from rest_framework.decorators import action
+
+    @action(methods=['post','get'], detail=True)
+    def denunciar (self, request, pk=None):
+        pass
+
+    '''
