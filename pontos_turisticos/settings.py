e48d7e9982f65b83e28b4e9d2ec1c9dade1adb93
Marcos Beraldo Barros <marcosberaldobarros@hotmail.com>
2021-08-18 16:53:02 -0400

    Gambiarra para mostrar imagens pelo link no desenvolvimento

--- pontos_turisticos/settings.py
+++ pontos_turisticos/settings.py
@@ -126,6 +126,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+MEDIA_ROOT = 'imagens'
+
+MEDIA_URL = '/media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
--- pontos_turisticos/urls.py
+++ pontos_turisticos/urls.py
@@ -20,6 +20,9 @@ from rest_framework import routers
 from core.api.viewsets import PontoTuristicoViewSet
 from atracoes.api.viewsets import AtracoesViewSet
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 router = routers.DefaultRouter()
 router.register(r'pontoturistico', PontoTuristicoViewSet, basename ='PontoTuristico')
 router.register(r'atracoes', AtracoesViewSet)
@@ -27,4 +30,4 @@ router.register(r'atracoes', AtracoesViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
