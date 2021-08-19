acddb98489ce281a1d839c20171e5e1bc520e085
Marcos Beraldo Barros <marcosberaldobarros@hotmail.com>
2021-08-19 00:06:48 -0400

    Trabalhando com Nested Serializers

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -1,4 +1,4 @@
-
+from atracoes.api.serializers import AtracaoSerializer
 
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.serializers import ModelSerializer
@@ -6,7 +6,9 @@ from core.models import PontoTuristico
 
 
 class PontoTuristicoSerializer(ModelSerializer):
+    atracoes = AtracaoSerializer(many=True)
+    
     class Meta:
         model = PontoTuristico
-        fields = ('id', 'nome', 'descricao', 'aprovado', 'foto')
+        fields = ('id', 'nome', 'descricao', 'aprovado', 'foto', 'atracoes')
     
